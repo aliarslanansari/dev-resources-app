abfe5c631cc88c7bac8f1da91d69962b4f94c93e
Ali Arslan <aliarslan1620@gmail.com>
2021-04-23 23:37:44 +0530

    removed unused variables

--- src/services/postServices.ts
+++ src/services/postServices.ts
@@ -2,7 +2,7 @@ import { ListPostsQueryVariables } from '../graphql/API'
 import { listPosts } from '../graphql/queries'
 import { graphQlOperation, GRAPHQL_OPERATION_TYPES } from '../utils/apiUtils'
 
-const { query, mutate } = GRAPHQL_OPERATION_TYPES
+const { query } = GRAPHQL_OPERATION_TYPES
 
 export const fetchPosts = (payload: ListPostsQueryVariables) =>
   graphQlOperation(query, listPosts, payload)
